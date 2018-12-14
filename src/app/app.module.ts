f80ddcf1c953582a425aa9101e56f9fc88ff2114
Shekhar Suman <shekhar_suman@outlook.com>
2018-12-14 21:05:38 +0530

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ClientsComponent } from './clients/clients.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialsComponent,
+    ClientsComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
